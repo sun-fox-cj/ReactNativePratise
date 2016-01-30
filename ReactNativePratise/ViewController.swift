0626c23f742636f459174f143ebd0f8e776292cf
Cjfire <cjfire@sina.cn>
2016-01-30 20:38:22 +0800

    项目初始化

---
+++ ReactNativePratise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactNativePratise
+//
+//  Created by Cjfire on 16/1/30.
+//  Copyright © 2016年 com.gozap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
