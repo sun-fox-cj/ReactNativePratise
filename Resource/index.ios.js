3356dfb1a2b9492f7d2bcad762e496fb245b77f5
Cjfire <cjfire@sina.cn>
2016-01-31 10:27:21 +0800

    添加搜索页

--- Resource/index.ios.js
+++ Resource/index.ios.js
@@ -1,23 +1,51 @@
 'use strict';
+
 var React = require('react-native');
+var SearchPage = require('./SearchPage')
+
 var {
   StyleSheet,
   Text,
-  View
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicatorIOS,
+  Image,
+  Component
 } = React;
 var styles = StyleSheet.create({
+   text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
   container: {
-    flex: 1,
-    backgroundColor: 'red'
+    flex: 1
   }
 });
-class SimpleApp extends React.Component {
+
+class SimpleApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This is a simple application.</Text>
-      </View>
-    )
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
   }
 }
+
+class Hello extends Component {
+  render() {
+    return (
+        <Text style={styles.text}>
+          Hello
+        </Text>
+      )
+  }
+}
+
 React.AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
